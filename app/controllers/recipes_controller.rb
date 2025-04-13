449e2f4e866c965ca8561164e3a3a3aef30bd722
mberry01 <matt.berry01221@gmail.com>
2025-04-13 02:19:37 +0000

    second commit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,65 @@
 class RecipesController < ApplicationController
+  before_action :require_owner, only: [:edit, :update, :destroy]
+
   def index
-    @recipes = Recipe.all  # Fetch all recipes for the main page
+    @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:id])  # Find recipe by ID
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = Current.user
+  
+    unless @recipe.image.attached?
+      flash.now[:alert] = "Please upload an image!"
+      render :new, status: :unprocessable_entity
+      return
+    end
+  
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      
+      flash.now[:alert] = "There was a problem creating the recipe: #{@recipe.errors.full_messages.join(', ')}"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      flash.now[:alert] = "There was a problem updating the recipe."
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :ingredients, :instructions, :image)
+  end
+
+  def require_owner
+    @recipe = Recipe.find(params[:id])
+    unless @recipe.user == Current.user
+      redirect_to recipes_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [ :new, :create ]
   resources :passwords, param: :token
-  resources :recipes, only: [ :index, :show ]
+  resources :recipes
+
 
   # Set the main page as root
   root "main#index"
 
   get "/main", to: "main#index"
-  get "/profile", to: "profile#index"
-  get "/post", to: "post#index"
+  get "/profile", to: "users#show", as: :profile
+  get "/post", to: "recipes#new"
   get "/settings", to: "settings#index"
 end
